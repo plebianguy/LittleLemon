836b19891f64a5c5321c0de4148cd76ec207ce36
plebianguy <ee.evliya@gmail.com>
2024-06-23 15:46:28 +0300

    added models and serializers

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'MenuAPI',
     'BookingAPI',
     'restaurant',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -77,9 +78,16 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'restaurant',  
+        'USER': 'admindjango',  
+        'PASSWORD': 'employee@123!',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
     }
 }
 
